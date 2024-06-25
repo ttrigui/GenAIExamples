620066a44814bcae7ed0bd044aae70f5ae09fab8
Bashir Mohd <bashir.mohammed@intel.com>
2024-06-24 21:54:06 -0700

    Update video-rag-ui.py

--- VideoRAGQnA/video-rag-ui.py
+++ VideoRAGQnA/video-rag-ui.py
@@ -118,7 +118,7 @@ instructions = [
     Ensure all information is distinct, accurate, and directly observable. 
     Provide a non-repetitive description of the actions performed by the person. 
     Exclude assumptions about age, a variety of items and background information.
-    When asked a question with a question mark, start the response with "yes" or "no" followed by a short, accurate explanation.
+    When asked a question repond with "yes" or "no" followed by a short, accurate explanation.
     Do not mention anything about a woman.
     """,
     
