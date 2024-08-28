6653123e8418be99688c373988b1e0ba401c6eef
intel-admin <parker.lischwe@intel.com>
2024-08-28 11:21:38 -0700

    HF token and meta-llama changes

--- VideoRAGQnA/int4_llama.py
+++ VideoRAGQnA/int4_llama.py
@@ -1,7 +1,11 @@
+# Code from https://github.com/intel-analytics/ipex-llm/blob/main/docs/mddocs/Overview/KeyFeatures/optimize_model.md
 from ipex_llm import optimize_model
 from transformers import LlamaForCausalLM
+import os
 
-model = LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', torch_dtype='auto', low_cpu_mem_usage=True)
+HF_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+print(HF_TOKEN)
+model = LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', torch_dtype='auto', low_cpu_mem_usage=True, token=HF_TOKEN)
 
 # Apply symmetric INT4 optimization
 model = optimize_model(model, low_bit="sym_int4")
