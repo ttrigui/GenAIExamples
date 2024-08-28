2265f5019b4322a7f90199b2a65e79866b384dd3
intel-admin <parker.lischwe@intel.com>
2024-08-28 08:17:48 -0700

    cleaning comments

--- VideoRAGQnA/int4_llama.py
+++ VideoRAGQnA/int4_llama.py
@@ -1,19 +1,10 @@
 from ipex_llm import optimize_model
 from transformers import LlamaForCausalLM
-#import pdb
+
 model = LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', torch_dtype='auto', low_cpu_mem_usage=True)
 
-# Apply symmetric INT8 optimization
+# Apply symmetric INT4 optimization
 model = optimize_model(model, low_bit="sym_int4")
 
 saved_dir='./llama-2-ipex-llm-4-bit'
 model.save_low_bit(saved_dir)
-
-#from ipex_llm.optimize import low_memory_init, load_low_bit
-#with low_memory_init(): # Fast and low cost by loading model on meta device
-#   model = LlamaForCausalLM.from_pretrained(saved_dir,
- #                                           torch_dtype="auto",
-  #                                          trust_remote_code=True)
-#pdb.set_trace()
-#model = load_low_bit(model, saved_dir) # Load the optimized model
-#print("Loaded optimized model")
